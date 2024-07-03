9baba4c5d5945f02e977ea8803a9d0714ab9c9b0
shreejal626 <42408689+shreejal626@users.noreply.github.com>
2024-07-03 16:50:41 +0800

    Update main.py

--- day-3-rollercoaster/main.py
+++ day-3-rollercoaster/main.py
@@ -11,16 +11,17 @@ if height >= 120:
   elif age <= 18:
     bill = 7
     print("Youth tickets are $7.")
+  elif age >= 45 and age <= 55:
+    print("Everything is going to be ok. Have a free ride on us!")
   else:
     bill = 12
     print("Adult tickets are $12.")
-
+  
   wants_photo = input("Do you want a photo taken? Y or N. ")
-  if wants_photo == "Y" or wants_photo == "y":
-    bill += 3    
-    
+  if wants_photo == "Y":
+    bill += 3
+  
   print(f"Your final bill is ${bill}")
 
 else:
   print("Sorry, you have to grow taller before you can ride.")
-  
